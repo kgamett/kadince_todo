5e216ceff75cee3f03bc4e40b8e163180bb72684
Kai <kgamett@chem.byu.edu>
2024-01-03 14:41:38 -0700

    Start of project

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+# from todo_app.models import 
 
 # Register your models here.
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from todo_app import views
+
 urlpatterns = [
     
 ]
