c37dc59cd17e35a1eb17dd61dc3f912dac7ed571
Kai <kgamett@chem.byu.edu>
2024-01-04 14:28:19 -0700

    added an edit and delete feature for the tasks

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add/', AddTaskView.as_view(), name='add_task'),
     path('complete/<int:task_id>/', CompleteTaskView.as_view(), name='complete_task'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('edit/<int:task_id>/', views.EditTaskView.as_view(), name='edit_task'),
+    path('delete/<int:task_id>/', views.DeleteTaskView.as_view(), name='delete_task'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 from .models import Task
 from .forms import TaskForm, SignUpForm
@@ -42,11 +43,33 @@ class AddTaskView(LoginRequiredMixin, View):
 
 
 class CompleteTaskView(LoginRequiredMixin, View):
-    def get(self, request, task_id):
-        task = Task.objects.get(pk=task_id)
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
         task.completed = True
         task.save()
-        return redirect('task_list')
+        return HttpResponseRedirect(reverse('task_list'))
+    
+class EditTaskView(LoginRequiredMixin, View):
+    def get(self, request, task_id):
+        task = Task.objects.get(pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'edit_task.html', {'form': form, 'task': task})
+    
+    def post(self, request, task_id):
+        task = Task.objects.get(pk=task_id, user=request.user)
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            edited_task = form.save(commit=False)
+            edited_task.user = request.user
+            edited_task.save()
+            return HttpResponseRedirect(reverse('task_list'))  # Redirect after editing
+        return render(request, 'edit_task.html', {'form': form, 'task': task})
+    
+class DeleteTaskView(LoginRequiredMixin, View):
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        task.delete()
+        return HttpResponseRedirect(reverse('task_list')) 
     
 class SignUpView(CreateView):
     template_name = 'registration/signup.html'
